de0a43402b2dc1f5e44c68f6e83dcb0c2a4a1378
Nic <nic@crearewebsolutions.com>
2022-06-08 11:56:45 -0400

    Remove unecessary header items

--- inc/classes/Bric.class.php
+++ inc/classes/Bric.class.php
@@ -20,6 +20,23 @@ class Bric {
 		
 		add_action( 'wp_enqueue_scripts', array( $this, 'easy_google_fonts_edits'), 999 );
 		
+
+		/**
+		 * Remove some crap in the WP head
+		 * 
+		 */
+		remove_action( 'wp_head', 'rest_output_link_wp_head' );
+		remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+		remove_action( 'wp_head', 'rsd_link');
+		remove_action( 'wp_head', 'wlwmanifest_link');
+		remove_action( 'wp_head', 'wp_shortlink_wp_head');
+
+		remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+	   
+		add_filter( 'xmlrpc_enabled', '__return_false' );
+
+
+
 	}
 	
 	
