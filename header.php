d3d1f0c6af1bd1a602aaa5a358c7554e9a53c91a
Nic <nic@crearewebsolutions.com>
2022-11-09 22:06:53 -0500

    Add head first action

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <!doctype html>
 <html lang="en-US">
   <head>
+	<?php
+		do_action( 'bric_head_first' );
+	?>
 	<meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
    	<?php 
